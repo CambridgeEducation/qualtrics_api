a8f17414b2be2655caf9877b9187cdff6f6afa2e
MatUrbanski <mateuszurbanski@yahoo.pl>
2015-06-25 15:10:57 +0200

    Add SurveyCreationDate attribute mapping to survey

--- lib/qualtrics_api/survey.rb
+++ lib/qualtrics_api/survey.rb
@@ -1,7 +1,7 @@
 module QualtricsAPI
 
   class Survey
-    attr_accessor :id, :name, :owner_id, :last_modified, :status
+    attr_accessor :id, :name, :owner_id, :last_modified, :status, :created_at
 
     def initialize(options = {})
       attributes_mappings.each do |key, qualtrics_key|
@@ -22,7 +22,8 @@ module QualtricsAPI
         :name => "name",
         :owner_id => "ownerId",
         :last_modified => "lastModified",
-        :status => "status"
+        :status => "status",
+        :created_at => "SurveyCreationDate"
       }
     end
   end
