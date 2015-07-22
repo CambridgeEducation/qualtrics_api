01a2b4b5d7b9eec7570f8140dd044c3adb94bb77
managr <marcin.naglik@gmail.com>
2015-07-22 10:25:45 +0200

    Allowing options to be passed.

--- lib/qualtrics_api/extensions/serializable_collection.rb
+++ lib/qualtrics_api/extensions/serializable_collection.rb
@@ -1,18 +1,15 @@
 module QualtricsAPI
   module Extensions
     module SerializableCollection
-
-      def as_json
+      def as_json(options = {})
         @all.map do |model|
-          model.as_json
+          model.as_json(options)
         end
       end
 
-      def to_json
-        as_json.to_json
+      def to_json(options = {})
+        as_json.to_json(options)
       end
-
     end
   end
 end
-
--- lib/qualtrics_api/extensions/serializable_model.rb
+++ lib/qualtrics_api/extensions/serializable_model.rb
@@ -1,15 +1,13 @@
 module QualtricsAPI
   module Extensions
     module SerializableModel
-
       def self.included(klass)
         klass.send :alias_method, :as_json, :attributes
       end
 
-      def to_json
-        attributes.to_json
+      def to_json(options = {})
+        attributes.to_json(options)
       end
-
     end
   end
 end
