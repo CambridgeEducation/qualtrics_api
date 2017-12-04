e98f8522e4edba9f2609ccdcc69588f0350b7f00
Yurui Zhang <eraclaymore@hotmail.com>
2017-12-04 12:57:40 -0500

    outputs api notice if any

--- lib/qualtrics_api/request_error_handler.rb
+++ lib/qualtrics_api/request_error_handler.rb
@@ -1,19 +1,34 @@
+require 'logger'
+
 module QualtricsAPI
   class RequestErrorHandler < Faraday::Response::Middleware
     def on_complete(env)
+      raise_http_errors(env[:status], env[:body])
+      show_notices(env[:body])
+    end
+
+    private
+
+    def raise_http_errors(code, body)
       case env[:status]
       when 404
         raise NotFoundError, "Not Found"
       when 400
-        raise BadRequestError, error_message(JSON.parse(env[:body]))
+        raise BadRequestError, error_message(JSON.parse(body))
       when 401
-        raise UnauthorizedError, error_message(JSON.parse(env[:body]))
+        raise UnauthorizedError, error_message(JSON.parse(body))
       when 500
-        raise InternalServerError, error_message(JSON.parse(env[:body]))
+        raise InternalServerError, error_message(JSON.parse(body))
       end
     end
 
-    private
+    def show_notices(body)
+      response = JSON.parse(body)
+      notice = response["meta"]["notice"]
+      if notice && notice.size > 0
+        STDERR.puts notice
+      end
+    end
 
     def error_message(response)
       meta = response["meta"]
